25498c96e2cfeae5a6c7029881ea483e59d5d577
vikas_ver <vikas.ver@ascendcorp.com>
2020-01-17 17:24:35 +0700

    Initial versin

---
+++ src/main/kotlin/com/example/KotlinWebFluxMongoTestContainer/KotlinWebFluxMongoTestContainerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.KotlinWebFluxMongoTestContainer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinWebFluxMongoTestContainerApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinWebFluxMongoTestContainerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/KotlinWebFluxMongoTestContainer/KotlinWebFluxMongoTestContainerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.KotlinWebFluxMongoTestContainer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinWebFluxMongoTestContainerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
